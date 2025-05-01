3851edde6fa12c4971bc6f1bac8c2be208b6d5bf
Tim van der Lippe <TimvdLippe@users.noreply.github.com>
2025-05-01 11:31:23 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -7,7 +7,7 @@ let respecConfig = {
   pubDomain: "testdir",
   shortName: "test",
   publishDate: "2023-06-21",
-  publishVersion: "1.2.4",
+  publishVersion: "1.2.5",
   
   previousPublishVersion: "0.0.2",
   previousPublishDate: "2022-06-24",
